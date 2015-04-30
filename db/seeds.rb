c1c55e55b77708052084e933863d1f08d3e72799
webninjataylor <webninjataylor@gmail.com>
2015-04-29 20:48:08 -0400

    First book seed with working dates.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Book.create(id:1, isbn:, title:, author_id:, genre:, abstract:, pages:, image_cover_url:, published_on:, total_in_library:, created_at:, updated_at:)
+Book.create(
+  isbn:               '054792822X',
+  title:              'The Hobbit',
+  author_id:          1,
+  genre:              'Fantasy',
+  abstract:           'Bilbo Baggins is a hobbit who enjoys a comfortable, unambitious life, rarely traveling any farther than his pantry or cellar. But his contentment is disturbed when the wizard Gandalf and a company of dwarves arrive on his doorstep one day to whisk him away on an adventure. They have launched a plot to raid the treasure hoard guarded by Smaug the Magnificent, a large and very dangerous dragon. Bilbo reluctantly joins their quest, unaware that on his journey to the Lonely Mountain he will encounter both a magic ring and a frightening creature known as Gollum.',
+  pages:              300,
+  image_cover_url:    'books/the_hobbit.jpg',
+  published_on:       Date.parse("18-09-2012"),
+  total_in_library:   5,
+  created_at:         DateTime.new(2015, 4, 29),
+  updated_at:         DateTime.new(2015, 4, 29)
+)
