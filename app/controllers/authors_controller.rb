ca1724bd5490d488cc967b45b468e53411b49f4f
Wellson Fung <wellson.fung@gmail.com>
2015-04-29 22:28:49 -0400

    Creating Authors

---
+++ app/controllers/authors_controller.rb
@@ -0,0 +1,15 @@
+class AuthorsController < ApplicationController
+
+  # GET /books
+  # GET /books.json
+  def index
+    @books = Book.all
+  end
+
+  # GET /books/1
+  # GET /books/1.json
+  def show
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   root :to => redirect('/books')
 
+  get "/authors" => "authors#index", as: 'authors'
+
   resources :books
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
