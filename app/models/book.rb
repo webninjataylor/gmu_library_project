3be0521aa6fedbd22110f4c41911501ed1b3cb2f
webninjataylor <webninjataylor@gmail.com>
2015-05-06 22:41:11 -0400

    Fixed model validation error.

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,7 +4,7 @@ class Book < ActiveRecord::Base
 
   validates :isbn, :title, :abstract, :pages, :genre, :published_on, :total_in_library, :author, presence: true
   validates :abstract, length: { minimum: 15 }
-  validates :pages, :total_in_library
+  validates :pages, :total_in_library,
      numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   # validates :genre, inclusion: { in: GENRES, message: "%{value} is not a valid genre" }
   # validates :author, inclusion: { in: AUTHORS, message: "%{value} is not a valid author" }
