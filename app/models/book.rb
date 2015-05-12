7738263a820a75a4bd3aa65913cd9e18748a8278
webninjataylor <webninjataylor@gmail.com>
2015-05-11 21:54:54 -0400

    Created GENRES constant and dropdoen on form.

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,11 +2,48 @@ class Book < ActiveRecord::Base
 
   belongs_to :author
 
+  GENRES = [
+    'Biography/Autobiography',
+    'Children\'s Novel',
+    'Classic',
+    'Comic/Graphic Novel',
+    'Coming of Age',
+    'Crime/Detective',
+    'Essay',
+    'Fable',
+    'Fairy Tale',
+    'Fanfiction',
+    'Fantasy',
+    'Fantasy Novel',
+    'Fiction Narrative',
+    'Fiction in Verse',
+    'Folklore',
+    'Historical Fiction',
+    'Horror',
+    'Humor',
+    'Legend',
+    'Magical Realism',
+    'Metafiction',
+    'Mystery',
+    'Mythology',
+    'Mythopoeia',
+    'Narrative Nonfiction',
+    'Novel',
+    'Realistic Fiction',
+    'Science Fiction',
+    'Short Story',
+    'Speech',
+    'Suspense/Thriller',
+    'Tall Tale',
+    'Textbook',
+    'Western'
+  ]
+
   validates :isbn, :title, :abstract, :pages, :genre, :published_on, :total_in_library, :author, presence: true
   validates :abstract, length: { minimum: 15 }
   validates :pages, :total_in_library,
      numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  # validates :genre, inclusion: { in: GENRES, message: "%{value} is not a valid genre" }
+  validates :genre, inclusion: { in: GENRES, message: "%{value} is not a valid genre" }
   # validates :author, inclusion: { in: AUTHORS, message: "%{value} is not a valid author" }
 
   has_many :reservations, dependent: :destroy
