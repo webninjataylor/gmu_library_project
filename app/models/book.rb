458abc23d190ea7a1fabfe177349c3ce54d95d02
webninjataylor <webninjataylor@gmail.com>
2015-05-11 22:00:27 -0400

    Fixed db migration glitch for book needing author (still need inclusion validation).

--- app/models/book.rb
+++ app/models/book.rb
@@ -39,7 +39,7 @@ class Book < ActiveRecord::Base
     'Western'
   ]
 
-  validates :isbn, :title, :abstract, :pages, :genre, :published_on, :total_in_library, :author, presence: true
+  validates :isbn, :title, :abstract, :pages, :genre, :published_on, :total_in_library, presence: true
   validates :abstract, length: { minimum: 15 }
   validates :pages, :total_in_library,
      numericality: { only_integer: true, greater_than_or_equal_to: 0 }
