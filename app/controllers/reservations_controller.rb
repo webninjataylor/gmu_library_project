0b3be3a3301abcf472158382987a38e8ed047fae
phutfut <wellson.fung@gmail.com>
2015-05-13 18:28:49 -0500

    fixed the destroy reservation method

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,11 +1,13 @@
 class ReservationsController < ApplicationController
 
-  # How do I get all the book information from only the user's reservation list?
+  before_action :set_user, only:[ :index, :create, :show]
+
+  # Get user from session
   def index
     @user = User.first
   end
 
-  # can I call index? get all reservations after a save to display all reservations
+  # Get user from session
   def create
     @book = Book.find_by_id(params[:book_id])
     # check to see if there is enough copies of the book
@@ -26,12 +28,17 @@ class ReservationsController < ApplicationController
 
   # Why is it trying to delete book also?
   # How to delete only the reservation and not the book?
+  # When returning a book, make sure to add it back to the collection
   def destroy
+    @reservation = Reservation.find(params[:id])
+    @reservation.book.update(total_in_library: @reservation.book.total_in_library + 1)
     @reservation.destroy
-    redirect_to reservations_path
+    redirect_to reservations_path, notice: "#{@reservation.book.title} has been returned"
   end
 
   # Show overdue books for user that's logged in
+  # Only show on the view the reservations with due date > today's date
+  # Query reservation for all due dates past today's date and pass that to the view
   def show
     @user = User.first
   end
@@ -41,4 +48,9 @@ class ReservationsController < ApplicationController
   def reservation_params
     params.require(:reservation).permit(:user_id, :book_id);
   end
+
+  def set_user
+
+  end
+
 end
